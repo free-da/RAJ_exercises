73fb3f3851121ce96fe73f8230563753ccdfedc3
Friederike Buchner <fry_github@hemio.de>
2022-05-21 16:40:25 +0200

    initial commit

---
+++ exercise_1.R
@@ -0,0 +1,19 @@
+# Exercise 1 (R)
+# Write a program to guess a number in between0 and 100!
+# Hence the computer invents the number and the user = you tries to guess it! 
+
+game <- function() {
+  computer <- floor(runif(1, min=0, max=101))
+ # computer <- 2
+  #print(computer)
+  me <- readline(prompt="Your number: ")
+  
+  if (computer== me) {
+    print("it's the same! you won.")
+  } else {
+    paste("Computer was ", computer, ". You lost.", sep="")
+  }
+}
+
+game()
+
